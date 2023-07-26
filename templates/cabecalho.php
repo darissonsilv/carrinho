2c05682629c4dff6e78106fb0df989327e43d9e7
darissonsilv <darissonsilva02@gmail.com>
2023-07-26 18:59:24 -0300

    26/07

---
+++ templates/cabecalho.php
@@ -0,0 +1,32 @@
+<?php
+
+?> 
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FOFO CAR</title>
+    <link rel="stylesheet" href="/carrinho/css/style.css">
+</head>
+
+<body> <!-- topo -->
+    <header>
+    <nav class="menu">
+                  
+                  <ul>
+          
+                       <li><a href="#" title="Home">Home</a></li>  
+                       <li><a href="#" title="Quem Somos">Quem Somos</a></li>
+                       <li><a href="#" title="Serviços">Serviços</a></li>  
+                       <li><a href="#" title="Fale Conosco">Fale Conosco</a></li>
+                            
+                  </ul>
+                          
+               </nav>
+    </header>
+    <!-- fim do topo -->
+
+    <main>
